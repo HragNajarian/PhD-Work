c0ff28e0fa08debf77e47ef98a1ba68152d3ad85
Hrag N <hrag.najarian@gmail.com>
2025-04-15 09:13:08 -0500

    Comments + bug testing

--- scripts/prep_conditions.sh
+++ scripts/prep_conditions.sh
@@ -1,29 +1,32 @@
-#!/bin/bash
+#! /bin/bash
+
+## IMPORTANT ##
+## First cd into your WPS directory and then run this script.
+## IMPORTANT ##
 
 # First get source bashrc_wrf
-source bashrc_wrf
+source ../bashrc_wrf
 
 # Run getera5.sh script within your WPS directory so that you get the .grib files
-./WPS/run_getera5.sh
+./run_getera5.sh
 
 # Make sure Vtable is symbolically linked 
-ln -sf WPS/ungrib/Variable_Tables/Vtable.ERA-interim.pl WPS/Vtable
+ln -sf ungrib/Variable_Tables/Vtable.ERA-interim.pl ./Vtable
 
 # This will make files ready (not entirely sure what this does) 
-./WPS/link_grib.csh WPS/ERA5*
+./link_grib.csh ./ERA5*
 
 # Execute ungrib.exe which puts the ERA5 data into an intermediate file format
-./WPS/ungrib.exe
+./ungrib.exe
 
 # We need another table to tell Metgrid which WRF version we're using
-ln -sf ./WPS/metgrid/METGRID.TBL.ARW ./WPS/METGRID.TBL
+ln -sf ./metgrid/METGRID.TBL.ARW ./METGRID.TBL
 
 # Execute metgrid.exe which puts the intermediate data into NetCDF format on the model grid, i.e., merging it with the output from Geogrid.
-./WPS/metgrid.exe
+./metgrid.exe
 
 
-
-# Error messages and their meaning:
+###### Error messages and their meaning:
 
 # If you get an error message when trying to run metgrid.exe that looks like WARNING: Field PRES has 
   # missing values, this is related to the domain of the ERA5 data not being far enough beyond the bound 
