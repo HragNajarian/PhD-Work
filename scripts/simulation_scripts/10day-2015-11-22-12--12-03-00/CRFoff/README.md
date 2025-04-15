3eb308ebcf1775630441040de97900006425fce7
Hrag N <hrag.najarian@gmail.com>
2025-04-15 09:21:36 -0500

    New simulation + renaming of directories

--- scripts/simulation_scripts/10day-2015-11-22-12--12-03-00/CRFoff/README.md
+++ scripts/simulation_scripts/10day-2015-11-22-12--12-03-00/CRFoff/README.md
@@ -5,10 +5,3 @@ Simply put, icloud is set to 0 to turn off cloud-radiative interactions
 namelists.input are specific to when in time the interaction is turned off
 
 These tests start from restart files which means the namelist.input also is edited to accomidate this
-
-
-
-
-
-
-
---
+++ scripts/simulation_scripts/10day-2015-12-09-12--12-20-00/WPS/run_getera5.sh
@@ -0,0 +1,74 @@
+#! /bin/bash -l
+
+mamba init
+mamba activate get_era5
+
+
+DATE1=20151209
+DATE2=20151220
+Nort=21
+West=79
+Sout=-21
+East=136
+
+
+YY1=`echo $DATE1 | cut -c1-4`
+MM1=`echo $DATE1 | cut -c5-6`
+DD1=`echo $DATE1 | cut -c7-8`
+YY2=`echo $DATE2 | cut -c1-4`
+MM2=`echo $DATE2 | cut -c5-6`
+DD2=`echo $DATE2 | cut -c7-8`
+
+
+YY='"year": ['
+for i in $(seq -f "%04g" $YY1 $YY2); do
+  YY+='"'$i'", '
+done
+# Remove the trailing comma and space, then add the closing bracket
+YY=${YY%, }']'
+
+MM='"month": ['
+for i in $(seq -f "%02g" $MM1 $MM2); do
+  MM+='"'$i'", '
+done
+# Remove the trailing comma and space, then add the closing bracket
+MM=${MM%, }']'
+
+DD='"day": ['
+for i in $(seq -f "%02g" $DD1 $DD2); do
+  DD+='"'$i'", '
+done
+# Remove the trailing comma and space, then add the closing bracket
+DD=${DD%, }']'
+
+
+# sed -e "s/YY/${YY}/g;s/MM/${MM}/g;s/DD/${DD}/g;s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;" GetERA5-sl.py > GetERA5-${DATE1}-${DATE2}-sl.py
+sed -e "s/YY/${YY}/g;s/MM/${MM}/g;s/DD/${DD}/g;s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;" GetERA5-pl.py > GetERA5-${DATE1}-${DATE2}-pl.py
+
+# python GetERA5-${DATE1}-${DATE2}-sl.py
+python GetERA5-${DATE1}-${DATE2}-pl.py
+
+
+
+# sed -e "s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;" GetERA5-sl.py > GetERA5-${DATE1}-${DATE2}-sl.py
+
+
+# python GetERA5-${DATE1}-${DATE2}-sl.py
+
+
+# sed -e "s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;" GetERA5-pl.py > GetERA5-${DATE1}-${DATE2}-pl.py
+
+
+# python GetERA5-${DATE1}-${DATE2}-pl.py
+
+#!/bin/bash -l
+
+
+# sed -e "s/YY/${YY}/g;s/MM/${MM}/g;s/DD/${DD}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;" GetERA5-sl.py > GetERA5-${DATE1}-${DATE2}-sl.py
+# sed -e "s/YY/${YY}/g;s/MM/${MM}/g;s/DD/${DD}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;" GetERA5-pl.py > GetERA5-${DATE1}-${DATE2}-pl.py
+
+# sed -e "s/YY/${YY}/g;s/MM/${MM}/g;s/DD/${DD}/g;s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;" GetERA5-sl.py > GetERA5-${DATE1}-${DATE2}-sl.py
+# sed -e "s/YY/${YY}/g;s/MM/${MM}/g;s/DD/${DD}/g;s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;" GetERA5-pl.py > GetERA5-${DATE1}-${DATE2}-pl.py
+
+# python GetERA5-${DATE1}-${DATE2}-sl.py
+# python GetERA5-${DATE1}-${DATE2}-pl.py
