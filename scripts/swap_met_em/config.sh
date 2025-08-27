ff69ce70e77c8b02250f3ed8825d625a01aed99b
Hrag N <hrag.najarian@gmail.com>
2025-08-27 15:01:31 -0500

    Previous variable values were to test the main script. Edits made now apply the script onto the enitre dataset.

--- scripts/swap_met_em/config.sh
+++ scripts/swap_met_em/config.sh
@@ -1,16 +1,16 @@
 ## Directory where met_files are located
 NC_DIR="/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/hragnajarian/met_em_files/og_files"
-SWAP_DIR="/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/hragnajarian/met_em_files/swapped_files/rh"
+SWAP_DIR="/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/hragnajarian/met_em_files/swapped_files/rh_wind"
 
 ## Variables to swap, i.e., "UU,VV,RH"
-VARS="RH"
+VARS="UU,VV,RH"
 
 ## Date ranges (inclusive, assumes hourly timesteps, can change it in the generate_hours function)
-START1="2015-12-10_00:00:00"
-END1="2015-12-10_03:00:00"
+START1="2015-11-22_12:00:00"
+END1="2015-12-03_12:00:00"
 
-START2="2015-12-19_00:00:00"
-END2="2015-12-19_03:00:00"
+START2="2015-12-09_12:00:00"
+END2="2015-12-20_12:00:00"
 
 ## Domains to process i.e., ("d01" "d02")
 DOMAINS=("d01" "d02")
